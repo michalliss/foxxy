b895e206c31fe2c94e00051bc59fdde74c2c2791
michalliss <michal.j.liss@gmail.com>
2024-08-28 21:51:59 +0200

    More flexible crud

--- reference/backend/src/db/Repository.scala
+++ reference/backend/src/db/Repository.scala
@@ -16,11 +16,11 @@ case class Repository(schema: Schema, dataSource: DataSource) {
 
   object users {
     def byUsername(username: String) = run(schema.users.filter(_.name == lift(username)).take(1)).map(_.headOption.map(_.to))
-    def c                            = crud(dataSource, schema.users)
+    def c                            = crudMap(dataSource, schema.users)
   }
 
   object todoItems {
-    def c                      = crud(dataSource, schema.todoItems)
+    def c                      = crudMap(dataSource, schema.todoItems)
     def byUserId(userId: UUID) = run(schema.todoItems.filter(_.userId == lift(userId))).map(_.map(_.to))
   }
 }
