564d24cd176121fdc33d6fec33ea64760581fc11
michalliss <michal.j.liss@gmail.com>
2025-09-27 16:35:34 +0200

    fix

--- reference/frontend_vite/main.js
+++ reference/frontend_vite/main.js
@@ -1,5 +1,6 @@
 import "@ui5/webcomponents/dist/Assets.js";
 import "@ui5/webcomponents-fiori/dist/Assets.js";
+import "@ui5/webcomponents/dist/features/InputElementsFormSupport.js";
 import {setTheme} from "@ui5/webcomponents-base/dist/config/Theme.js";
 import './style.css'
 import './webapp/main.js'
