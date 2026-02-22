e2e4c2067ee8d2b5f0db83e567f6b0c3f5b1d55f
michalliss <michal.j.liss@gmail.com>
2026-02-22 21:24:47 +0100

    .

--- reference/frontend/src/services/MyHttpClient.scala
+++ reference/frontend/src/services/MyHttpClient.scala
@@ -8,6 +8,32 @@ import sttp.tapir.Endpoint
 import zio.*
 import zio.json.JsonCodec
 
+object HttpCaps {
+
+  trait TapirEndpointRunner { self =>
+    def apply[In, Err, Out](e: Endpoint[String, In, Err, Out, Any]): (In => Task[Either[Err, Out]])
+
+    object extensions {
+      extension [In, Err, Out](endpoint: Endpoint[String, In, Err, Out, Any]) def sendSecure = { self.apply(endpoint) }
+    }
+  }
+
+  case class HttpClientImpl(storage: Storage, router: Router[Page], authSerivce: AuthService) extends TapirEndpointRunner {
+
+    def fetchToken  = storage.get[String]("token")
+    def backendHost = "localhost:5004"
+
+    override def apply[In, Err, Out](e: Endpoint[String, In, Err, Out, Any]): In => ZIO[Any, Throwable, Either[Err, Out]] =
+      foxxy.frontend.utils
+        .sendSecure(e)(Uri(backendHost))(fetchToken.getOrElse("no_token"))
+        .andThen(x =>
+          x
+            .tapSome { case Left(Unauthorized(msg)) => ZIO.attempt { authSerivce.logout; router.pushState(Page.Login) } }
+            .mapError(x => Throwable(""))
+        )
+  }
+}
+
 case class MyHttpClient(storage: Storage, router: Router[Page], authSerivce: AuthService) {
 
   def fetchToken  = storage.get[String]("token")
